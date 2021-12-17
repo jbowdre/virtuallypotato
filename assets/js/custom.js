fbf3d7b64bb137620b3c8270bd31bbfb69ff90e2
John Bowdre <john@bowdre.net>
2021-12-17 15:49:08 -0600

    implement collapsible table of contents at the top of the post body, remove it from the sidebar

---
+++ assets/js/custom.js
@@ -0,0 +1,13 @@
+// Make the collapsible content accessible
+let myLabels = document.querySelectorAll('.lbl-toggle');
+
+Array.from(myLabels).forEach(label => {
+  label.addEventListener('keydown', e => {
+    // 32 === spacebar
+    // 13 === enter
+    if (e.which === 32 || e.which === 13) {
+      e.preventDefault();
+      label.click();
+    };
+  });
+});
